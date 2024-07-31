816bafe2c56936d388785429b0730e5b5619d0bd
Chico <leeser.lionel@gmail.com>
2024-07-31 19:49:38 +0200

    Exported shortcuts...

--- shortcuts.go
+++ shortcuts.go
@@ -6,35 +6,48 @@ import (
 )
 
 var (
-	shortcutCtrlA         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyA, fyne.KeyModifierControl)
-	shortcutCtrlB         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyB, fyne.KeyModifierControl)
-	shortcutCtrlH         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyH, fyne.KeyModifierControl)
-	shortcutCtrlO         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyO, fyne.KeyModifierControl)
-	shortcutCtrlS         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyS, fyne.KeyModifierControl)
-	shortcutCtrlN         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyN, fyne.KeyModifierControl)
-	shortcutCtrlW         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyW, fyne.KeyModifierControl)
-	shortcutCtrlI         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyI, fyne.KeyModifierControl)
-	shortcutCtrlD         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyD, fyne.KeyModifierControl)
-	shortcutCtrlT         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyT, fyne.KeyModifierControl)
-	shortcutCtrlE         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyE, fyne.KeyModifierControl)
-	shortcutCtrlP         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyP, fyne.KeyModifierControl)
-	shortcutCtrlF         *desktop.CustomShortcut = createCustomShortcut(fyne.KeyF, fyne.KeyModifierControl)
-	shortcutCtrlUp        *desktop.CustomShortcut = createCustomShortcut(fyne.KeyUp, fyne.KeyModifierControl)
-	shortcutCtrlDown      *desktop.CustomShortcut = createCustomShortcut(fyne.KeyDown, fyne.KeyModifierControl)
-	shortcutCtrlTab       *desktop.CustomShortcut = createCustomShortcut(fyne.KeyTab, fyne.KeyModifierControl)
-	shortcutCtrlEnter     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyEnter, fyne.KeyModifierControl)
-	shortcutCtrlShiftTab  *desktop.CustomShortcut = createCustomShortcut(fyne.KeyTab, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftN    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyN, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftI    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyI, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftW    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyW, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftO    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyO, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftE    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyE, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftT    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyT, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftF    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyF, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftR    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyR, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftV    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyV, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftUp   *desktop.CustomShortcut = createCustomShortcut(fyne.KeyUp, fyne.KeyModifierControl|fyne.KeyModifierShift)
-	shortcutCtrlShiftDown *desktop.CustomShortcut = createCustomShortcut(fyne.KeyDown, fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlA     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyA, fyne.KeyModifierControl)
+	ShortcutCtrlB     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyB, fyne.KeyModifierControl)
+	ShortcutCtrlH     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyH, fyne.KeyModifierControl)
+	ShortcutCtrlO     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyO, fyne.KeyModifierControl)
+	ShortcutCtrlS     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyS, fyne.KeyModifierControl)
+	ShortcutCtrlN     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyN, fyne.KeyModifierControl)
+	ShortcutCtrlW     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyW, fyne.KeyModifierControl)
+	ShortcutCtrlI     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyI, fyne.KeyModifierControl)
+	ShortcutCtrlD     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyD, fyne.KeyModifierControl)
+	ShortcutCtrlT     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyT, fyne.KeyModifierControl)
+	ShortcutCtrlE     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyE, fyne.KeyModifierControl)
+	ShortcutCtrlP     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyP, fyne.KeyModifierControl)
+	ShortcutCtrlF     *desktop.CustomShortcut = createCustomShortcut(fyne.KeyF, fyne.KeyModifierControl)
+	ShortcutCtrlUp    *desktop.CustomShortcut = createCustomShortcut(fyne.KeyUp, fyne.KeyModifierControl)
+	ShortcutCtrlDown  *desktop.CustomShortcut = createCustomShortcut(fyne.KeyDown, fyne.KeyModifierControl)
+	ShortcutCtrlTab   *desktop.CustomShortcut = createCustomShortcut(fyne.KeyTab, fyne.KeyModifierControl)
+	ShortcutCtrlEnter *desktop.CustomShortcut = createCustomShortcut(fyne.KeyEnter, fyne.KeyModifierControl)
+
+	ShortcutCtrlShiftTab *desktop.CustomShortcut = createCustomShortcut(fyne.KeyTab,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftN *desktop.CustomShortcut = createCustomShortcut(fyne.KeyN,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftI *desktop.CustomShortcut = createCustomShortcut(fyne.KeyI,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftW *desktop.CustomShortcut = createCustomShortcut(fyne.KeyW,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftO *desktop.CustomShortcut = createCustomShortcut(fyne.KeyO,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftE *desktop.CustomShortcut = createCustomShortcut(fyne.KeyE,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftT *desktop.CustomShortcut = createCustomShortcut(fyne.KeyT,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftF *desktop.CustomShortcut = createCustomShortcut(fyne.KeyF,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftR *desktop.CustomShortcut = createCustomShortcut(fyne.KeyR,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftV *desktop.CustomShortcut = createCustomShortcut(fyne.KeyV,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftUp *desktop.CustomShortcut = createCustomShortcut(fyne.KeyUp,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
+	ShortcutCtrlShiftDown *desktop.CustomShortcut = createCustomShortcut(fyne.KeyDown,
+		fyne.KeyModifierControl|fyne.KeyModifierShift)
 )
 
 // createCustomShortcut creates and return a pointer to a new custom shortcut
