bc88c74dd108cc730198faff8e2a2d611821a981
Chico <leeser.lionel@gmail.com>
2024-08-30 14:51:21 +0200

    Added Window to engine

---
+++ engine.go
@@ -0,0 +1,45 @@
+package komok
+
+import (
+	"fyne.io/fyne/v2"
+	"github.com/chicogamedev/komok/manager"
+)
+
+var AppEngine *Engine
+
+type Engine struct {
+	Widgets         map[string]fyne.Widget
+	Systems         map[string]System
+	MenuManager     *manager.MenuManager
+	ShortcutManager *manager.ShortcutsManager
+	Window          fyne.Window
+}
+
+type System interface {
+	HandleSignal(e *Engine, s string)
+}
+
+func InitAppEngine(window fyne.Window) {
+	sm := manager.NewShortcutsManager()
+	AppEngine = &Engine{
+		Widgets:         make(map[string]fyne.Widget),
+		Systems:         make(map[string]System),
+		ShortcutManager: sm,
+		MenuManager:     manager.NewMenuManager(window, sm),
+		Window:          window,
+	}
+}
+
+func (k *Engine) RegisterWidget(code string, widget fyne.Widget) {
+	k.Widgets[code] = widget
+}
+
+func (k *Engine) RegisterSystem(code string, system System) {
+	k.Systems[code] = system
+}
+
+func (k *Engine) SendSignal(s string) {
+	for id := range k.Systems {
+		k.Systems[id].HandleSignal(k, s)
+	}
+}
